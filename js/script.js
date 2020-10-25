84839f800711ba25b626f5ea827ae780983f7c27
shahbaz <sadiksojib@gmail.com>
2020-10-26 02:28:58 +0600

    add responsive UI and fixd some bug

--- js/script.js
+++ js/script.js
@@ -1,37 +1,35 @@
 
 
-
-
-
-
-
+//select dom element
 const currencyOne = document.getElementById('select-currency-one');
 const inputOne= document.getElementById('input-one');
 const currencyTwo = document.getElementById('select-currency-two');
 const inputTwo = document.getElementById('input-2');
 const rateValue = document.getElementById('rate-value');
 
-
+//add eventlistener to selected dom element
 currencyOne.addEventListener('change', convator);
 currencyTwo.addEventListener('change', convator);
 inputOne.addEventListener('input', convator);
 inputTwo.addEventListener('input', convator);
 
+//call api and calculate functionn
 async function convator(){
     const currencyValueOne = currencyOne.value;
     const currencyValueTwo = currencyTwo.value;
-    console.log(currencyValueTwo);
+    //console.log(currencyValueTwo);
 
         const result = await fetch(`https://api.exchangerate-api.com/v4/latest/${currencyValueOne}`);
         const data = await result.json();
-        console.log(data);
+        //console.log(data);
         const rates = data.rates[currencyValueTwo];
 
-        console.log(rates);
+        //console.log(rates);
         rateValue.innerText = (`1 ${currencyValueOne} = ${rates} ${currencyValueTwo}`);
 
         inputTwo.value = (rates * inputOne.value).toFixed(2);
 
 }
 
+//initial call
 convator();
