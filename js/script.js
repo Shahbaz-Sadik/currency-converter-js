b04a396bcef17f54dbbfe430c42f1763614f03a6
shahbaz <sadiksojib@gmail.com>
2020-10-27 18:50:32 +0600

    improve some issue and fixd some bug

--- js/script.js
+++ js/script.js
@@ -4,15 +4,16 @@
 const currencyOne = document.getElementById('select-currency-one');
 const inputOne = document.getElementById('input-one');
 const currencyTwo = document.getElementById('select-currency-two');
-const inputTwo = document.getElementById('input-2');
+
+const resultShow= document.getElementById('result');
 const rateValue = document.getElementById('rate-value');
 const swapButton = document.getElementById('swap-btn');
 
 //add eventlistener to selected dom element
-currencyOne.addEventListener('change', convator);
-currencyTwo.addEventListener('change', convator);
-inputOne.addEventListener('input', convator);
-inputTwo.addEventListener('input', convator);
+currencyOne.addEventListener('change', calculateCurrency);
+currencyTwo.addEventListener('change', calculateCurrency);
+inputOne.addEventListener('input', calculateCurrency);
+//inputTwo.addEventListener('input', convator);
 
 
 //swap button for the select value
@@ -21,12 +22,13 @@ swapButton.addEventListener('click', () => {
     currencyOne.value = currencyTwo.value;
     currencyTwo.value = temp;
 
-    convator();
+    //convator();
+    calculateCurrency();
 
 });
 
 //call api and calculate functionn
-async function convator() {
+async function fetchCurrencyRate() {
     const currencyValueOne = currencyOne.value;
     const currencyValueTwo = currencyTwo.value;
     //console.log(currencyValueTwo);
@@ -35,16 +37,29 @@ async function convator() {
         const data = await result.json();
         //console.log(data);
         const rates = data.rates[currencyValueTwo];
-
         //console.log(rates);
+
+        
         rateValue.innerText = (`1 ${currencyValueOne} = ${rates} ${currencyValueTwo}`);
 
-        inputTwo.value = (rates * inputOne.value).toFixed(2);
+        //resultShow.innerText = (rates * inputOne.value).toFixed(2);
+
+        return rates;
+
     } catch (error) {
         alert(error);
     }
 
 }
 
+async function calculateCurrency() {
+    const getRates = await fetchCurrencyRate();
+
+    resultShow.innerText = (getRates * inputOne.value).toFixed(2);
+    //console.log(getRates);
+}
+
+
+
 //initial call
-convator();
+calculateCurrency();
